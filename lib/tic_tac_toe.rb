66fb814d9eb8a19d743d5d55d211d49e08dbbd37
Allison Howard <alliemhow@gmail.com>
2018-08-20 21:21:34 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -18,7 +18,7 @@ class TicTacToe
     while !over?
     end
     if won?
-      puts "Congratulations #{winner!}"
+      puts "Congratulations #{winner}!"
     elsif draw?
       puts "Cat's Game!"
     end
